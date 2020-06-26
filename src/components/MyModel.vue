24d93b0b00f739437a75e4bc46ddb7c53c751b0f
Einere <kjwsx23@gmail.com>
2020-06-26 19:02:17 +0900

    feat: add @Model
    그러나 해당 데코레이터는 정말 쓸모없어 보인다..
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ src/components/MyModel.vue
@@ -0,0 +1,40 @@
+<template>
+  <input type="checkbox" v-model="isChecked" @change="handleClick"/>
+</template>
+
+<script lang="ts">
+// @Model 대상 상태는 props로 취급된다.
+// 즉, MyModel의 isChecked 상태는 부모로부터 받은 prop이다.
+// 부모의 리렌더링때 마다 값이 변경될 수 있기 때문에 props를 직접적으로 변경하는 것은 권장되지 않는다.
+// 따라서 @Model 대신 @Emit을 이용해 부모의 상태를 조작하는 것이 더 나은 방법이다.
+
+/* export default {
+  name: 'MyModel',
+  model: {
+    prop: 'isChecked',
+    event: 'change',
+  },
+  props: {
+    isChecked: Boolean,
+  },
+}; */
+
+import {
+  Vue, Component, Model, Emit,
+} from 'vue-property-decorator';
+
+@Component
+export default class MyModel extends Vue {
+  @Model('change', { type: Boolean }) readonly isChecked!: boolean;
+
+  @Emit('checkboxChanged')
+  // eslint-disable-next-line class-methods-use-this
+  handleClick(e: Event) {
+    return e.target?.checked;
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,26 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <Message :parent-message="parentMessage" @counter="counter"/>
-    <p>count : {{count}}</p>
+    <p>count : {{ count }}</p>
+    <MyModel v-model="isChecked" @checkboxChanged="handleCheckboxChange" />
+    <p v-if="isChecked">checked!</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Provide } from 'vue-property-decorator';
 import Message from '@/components/Message.vue';
+import MyModel from '@/components/MyModel.vue';
 
 @Component({
-  components: { Message },
+  components: { Message, MyModel },
 })
 export default class Home extends Vue {
   private parentMessage = 'hello';
 
   private count = 0;
+
+  private isChecked = false;
 
   // eslint-disable-next-line class-methods-use-this
   counter(n: number) {
@@ -25,5 +30,9 @@ export default class Home extends Vue {
   }
 
   @Provide() provided = 'provided';
+
+  handleCheckboxChange(isChecked: boolean) {
+    this.isChecked = isChecked;
+  }
 }
 </script>
