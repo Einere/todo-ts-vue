cd4eb9fcf6b2d634dd04eafa2f8e11131568816c
Einere <kjwsx23@gmail.com>
2020-06-28 19:03:41 +0900

    typo: modify for readability
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,10 @@
   <header>
     <h1>Todo Ts Vue</h1>
     <nav>
-      <router-link to="/">All</router-link> |
-      <router-link to="/progress">Progress</router-link> |
+      <router-link to="/">All</router-link>
+       |
+      <router-link to="/progress">Progress</router-link>
+       |
       <router-link to="/done">Done</router-link>
     </nav>
   </header>
